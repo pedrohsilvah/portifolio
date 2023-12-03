14a67ec71d6968da53b15cc4e6d6f5e7a30e62c2
Pedro Henrique  da Silva Santos <124065912+pedrohsilvah@users.noreply.github.com>
2023-12-03 12:20:01 -0300

    animando navbar

--- js/mobile-navbar.js
+++ js/mobile-navbar.js
@@ -1,33 +1,44 @@
 class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active";
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
 
-        this.handleCLick = this.handleCLick.bind(this);
-    }
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    handleClick() {
-        console.log(this)
-        this.navList.ClassList.toggle(this.activeClass);
-    }
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
 
-    addClickEvent() {
-        this.mobileMenu.addEventListener('click', this.handleClick)
-    }
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
 
-    init() {
-        if(this.mobileMenu) {
-            this.addClickEvent();
-        }
-        return this;
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
     }
+    return this;
+  }
 }
 
 const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    "nav-lis",
-    ".nav-list li"
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
 );
 mobileNavbar.init();
